bb354705265bfe3caeba12bea9ed739d5f6522aa
Su, Jian-Hau <jenhausu@icloud.com>
2015-07-17 22:27:08 +0800

    member page

--- app/controllers/one_controller.rb
+++ app/controllers/one_controller.rb
@@ -2,4 +2,8 @@ class OneController < ApplicationController
     def about
         render 'one/About.html.erb'
     end
+
+    def member
+        render 'one/member'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     root 'one#home'
 
     get 'about', to: 'one#about'
+    post 'member', to: 'one#member'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
