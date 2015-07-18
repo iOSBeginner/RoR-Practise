cd9ed867ee98821b4f8e9e363912e0bf08f92f85
Su, Jian-Hau <jenhausu@icloud.com>
2015-07-18 18:05:27 +0800

    "sign up" page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
     root 'one#home'
 
     get 'about', to: 'one#about'
+    
     post 'member', to: 'one#member'
+    get 'signup', to: 'one#signup'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
